86729eb6bc093fec3a37e60988c6069d92f2c4da
gromoff97 <gromoff97@mail.ru>
2018-03-25 22:48:40 +0300

    smth3

--- src/course/service/LostFoundService.java
+++ src/course/service/LostFoundService.java
@@ -1,28 +1,45 @@
 package course.service;
 
 import course.entity.EntityLostFound;
+import org.apache.tools.ant.taskdefs.EchoXML;
 
 import javax.persistence.EntityManager;
 import java.sql.Date;
+import java.util.Collection;
 
 public class LostFoundService {
 
-    public void createMessage(String message) {
+    public boolean createMessage(String message) {
         EntityManager em = EntityService.getEntityManager();
         Date date = new Date(new java.util.Date().getTime());
         EntityLostFound lostFound = new EntityLostFound(message, date);
         em.getTransaction().begin();
-        em.persist(lostFound);
+        try {
+            em.persist(lostFound);
+        } catch (Exception e) {
+            return false;
+        }
         em.getTransaction().commit();
         em.close();
+        return true;
     }
 
-    public void deleteMessage(int id){
+    public boolean deleteMessage(int id){
         EntityManager em = EntityService.getEntityManager();
         em.getTransaction().begin();
-        em.createQuery("DELETE FROM EntityLostFound WHERE id = :id")
+        try {
+            em.createQuery("DELETE FROM EntityLostFound WHERE id = :id")
                 .setParameter("id", id).executeUpdate();
+        } catch (Exception e) {
+            return false;
+        }
         em.getTransaction().commit();
         em.close();
+        return true;
+    }
+
+    public Collection<EntityLostFound> getMessages(){
+        EntityManager em = EntityService.getEntityManager();
+        return em.createQuery("SELECT lf FROM EntityLostFound lf",EntityLostFound.class).getResultList();
     }
 }
--- src/course/service/NewsService.java
+++ src/course/service/NewsService.java
@@ -4,35 +4,58 @@ import course.entity.EntityNews;
 
 import javax.persistence.EntityManager;
 import java.sql.Date;
+import java.util.Collection;
 
 public class NewsService {
 
-    public void createPost(String content) {
+    public boolean createPost(String content) {
         EntityManager em = EntityService.getEntityManager();
         Date date = new Date(new java.util.Date().getTime());
         EntityNews post = new EntityNews(content, date);
         em.getTransaction().begin();
-        em.persist(post);
+        try {
+            em.persist(post);
+        } catch (Exception e) {
+            return false;
+        }
         em.getTransaction().commit();
         em.close();
+        return true;
     }
 
-    public void deletePost(int id){
+    public boolean deletePost(int id){
         EntityManager em = EntityService.getEntityManager();
         em.getTransaction().begin();
-        em.createQuery("DELETE FROM EntityNews WHERE id = :id")
+        try {
+            em.createQuery("DELETE FROM EntityNews WHERE id = :id")
                 .setParameter("id", id).executeUpdate();
+        } catch (Exception e) {
+            return false;
+        }
         em.getTransaction().commit();
         em.close();
+        return true;
     }
 
-    public void changePostContent(int id, String newContent) {
+    public boolean changePostContent(int id, String newContent) {
         EntityManager em = EntityService.getEntityManager();
         em.getTransaction().begin();
-        EntityNews post = em.createQuery("SELECT n FROM EntityNews n WHERE n.id = :id", EntityNews.class)
-                .setParameter("id", id).getSingleResult();
-        post.setContent(newContent);
+        try {
+            em.createQuery("SELECT n FROM EntityNews n WHERE n.id = :id", EntityNews.class)
+                    .setParameter("id", id)
+                    .getSingleResult()
+                    .setContent(newContent);
+        } catch (Exception e) {
+            return false;
+        }
         em.getTransaction().commit();
         em.close();
+        return true;
     }
+
+    public Collection<EntityNews> getPosts(){
+        EntityManager em = EntityService.getEntityManager();
+        return em.createQuery("SELECT n FROM EntityNews n",EntityNews.class).getResultList();
+    }
+
 }
