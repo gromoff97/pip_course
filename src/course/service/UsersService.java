489bbe34d3fe51ca38b70019410b3b9b77ff5333
gromoff97 <gromoff97@mail.ru>
2018-03-26 00:40:39 +0300

    UsersService was edited

--- src/course/service/UsersService.java
+++ src/course/service/UsersService.java
@@ -1,20 +1,117 @@
 package course.service;
 
+import course.entity.EntityPermissionTypes;
+import course.entity.EntityUsers;
+
+import javax.persistence.EntityManager;
+import java.sql.Date;
+import java.util.Collection;
+
 public class UsersService {
 
-    public void createUser(){
-
+    public boolean createUser(String firstName, String lastName, Date birthDate, String eMail, EntityPermissionTypes userPermissionType){
+        EntityManager em = EntityService.getEntityManager();
+        EntityUsers user = new EntityUsers(firstName,lastName,birthDate,eMail,userPermissionType);
+        em.getTransaction().begin();
+        try{
+            em.persist(user);
+        } catch (Exception e) {
+            return false;
+        }
+        em.getTransaction().commit();
+        em.close();
+        return true;
     }
 
-    public void deleteUser(){
-
+    public boolean deleteUser(int id){
+        EntityManager em = EntityService.getEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.createQuery("DELETE FROM EntityUsers u WHERE u.id = :id")
+                    .setParameter("id", id).executeUpdate();
+        } catch (Exception e){
+            return false;
+        }
+        em.getTransaction().commit();
+        em.close();
+        return true;
     }
 
-    public void changeUserEmail(){
-
+    public boolean changeUserEmail(int userId, String nowUserEmail){
+        EntityManager em = EntityService.getEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.createQuery("SELECT u FROM EntityUsers u WHERE u.id = :id", EntityUsers.class)
+                    .setParameter("id",userId)
+                    .getSingleResult()
+                    .seteMail(nowUserEmail);
+        } catch (Exception e) {
+            return false;
+        }
+        em.getTransaction().commit();
+        em.close();
+        return false;
     }
 
-    public void changeUserFirstName(){
-
+    public boolean changeUserFirstName(int userId, String nowFirstName){
+        EntityManager em = EntityService.getEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.createQuery("SELECT u FROM EntityUsers u WHERE u.id = :id", EntityUsers.class)
+                    .setParameter("id",userId)
+                    .getSingleResult()
+                    .setFirstName(nowFirstName);
+        } catch (Exception e) {
+            return false;
+        }
+        em.getTransaction().commit();
+        em.close();
+        return false;
     }
+
+    public boolean changeUserLastName(int userId, String nowLastName){
+        EntityManager em = EntityService.getEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.createQuery("SELECT u FROM EntityUsers u WHERE u.id = :id", EntityUsers.class)
+                    .setParameter("id",userId)
+                    .getSingleResult()
+                    .setLastName(nowLastName);
+        } catch (Exception e) {
+            return false;
+        }
+        em.getTransaction().commit();
+        em.close();
+        return false;
+    }
+
+    public boolean changeUserBalance(int userId, int nowBalance){
+        EntityManager em = EntityService.getEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.createQuery("SELECT u FROM EntityUsers u WHERE u.id = :id", EntityUsers.class)
+                    .setParameter("id",userId)
+                    .getSingleResult()
+                    .setBalance(nowBalance);
+        } catch (Exception e) {
+            return false;
+        }
+        em.getTransaction().commit();
+        em.close();
+        return false;
+    }
+
+    public Collection<EntityUsers> getUsers() {
+        EntityManager em = EntityService.getEntityManager();
+        return em.createQuery("SELECT l FROM EntityUsers l",EntityUsers.class).getResultList();
+    }
+
+    public EntityUsers getUserByEmail(String Email){
+        EntityManager em = EntityService.getEntityManager();
+        return em.createQuery("SELECT l FROM EntityUsers l where l.eMail = :mail",EntityUsers.class)
+                .setParameter("mail",Email)
+                .getSingleResult();
+    }
+
+
 }
