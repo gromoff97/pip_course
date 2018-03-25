bf885a847be228130bf8eb5cc49a2f2ef7cc767e
gromoff97 <gromoff97@mail.ru>
2018-03-25 21:37:21 +0300

    StationService was created

---
+++ src/course/service/StationService.java
@@ -0,0 +1,20 @@
+package course.service;
+
+public class StationService {
+
+    public void createStation() {
+
+    }
+
+    public void deleteStation() {
+
+    }
+
+    public void changeStationName(){
+
+    }
+
+    public void changeStationState(){
+
+    }
+}
