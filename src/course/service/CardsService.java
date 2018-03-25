e21c44876d8d7ecabbde819f2d88571ee4bbf4f7
SpaceStar <spacestar.ru@gmail.com>
2018-03-26 01:36:54 +0300

    add CardsService

--- src/course/service/CardsService.java
+++ src/course/service/CardsService.java
@@ -2,21 +2,59 @@ package course.service;
 
 
 import course.entity.EntityCardTypes;
+import course.entity.EntityCards;
 import course.entity.EntityUsers;
 
-import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import java.util.Collection;
 
 public class CardsService {
 
-    public void createCard(int userId, int typeId, int balance) {
-
+    public boolean createCard(EntityUsers user, EntityCardTypes type) {
+        EntityManager em = EntityService.getEntityManager();
+        EntityCards card = new EntityCards(user, type);
+        em.getTransaction().begin();
+        try {
+            em.persist(card);
+        } catch (Exception e) {
+            return false;
+        }
+        em.getTransaction().commit();
+        em.close();
+        return true;
     }
 
-    public void deleteCard(int cardId) {
-
+    public boolean deleteCard(int cardId) {
+        EntityManager em = EntityService.getEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.createQuery("DELETE FROM EntityCards WHERE id = :id")
+                    .setParameter("id", cardId).executeUpdate();
+        } catch (Exception e){
+            return false;
+        }
+        em.getTransaction().commit();
+        em.close();
+        return true;
     }
 
-    public void changeCardType(int cardId, int id ){
+    public boolean changeCardType(int cardId, EntityCardTypes newType) {
+        EntityManager em = EntityService.getEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.createQuery("SELECT c FROM EntityCards c WHERE c.id = :id", EntityCards.class)
+                    .setParameter("id", cardId).getSingleResult().setCardType(newType);
+        } catch (Exception e) {
+            return false;
+        }
+        em.getTransaction().commit();
+        em.close();
+        return true;
+    }
 
+    public Collection<EntityCards> getCards() {
+        EntityManager em = EntityService.getEntityManager();
+        return em.createQuery("SELECT c FROM EntityCards c", EntityCards.class)
+                .getResultList();
     }
 }
