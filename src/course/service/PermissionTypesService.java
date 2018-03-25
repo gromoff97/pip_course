f9c90c4355a3b9eeb38368f81506f5be4159895b
gromoff97 <gromoff97@mail.ru>
2018-03-25 22:39:44 +0300

    smth2

--- src/course/service/PermissionTypesService.java
+++ src/course/service/PermissionTypesService.java
@@ -4,36 +4,64 @@ package course.service;
 import course.entity.EntityPermissionTypes;
 
 import javax.persistence.EntityManager;
+import java.util.Collection;
 
 public class PermissionTypesService {
 
-    public void createType(String typeName){
+    public boolean createType(String typeName){
         EntityManager em = EntityService.getEntityManager();
         EntityPermissionTypes permType = new EntityPermissionTypes(typeName);
         em.getTransaction().begin();
-        em.persist(permType);
+        try {
+            em.persist(permType);
+        } catch (Exception e) {
+            return false;
+        }
         em.getTransaction().commit();
         em.close();
+        return true;
     }
 
-    public void deleteType(String typeName){
+    public boolean deleteType(String typeName){
         EntityManager em = EntityService.getEntityManager();
         em.getTransaction().begin();
-        em.createQuery("DELETE FROM EntityPermissionTypes WHERE name = :name")
-                .setParameter("name",typeName).executeUpdate();
+        try {
+            em.createQuery("DELETE FROM EntityPermissionTypes WHERE name = :name")
+                    .setParameter("name",typeName).executeUpdate();
+        } catch (Exception e) {
+            return false;
+        }
         em.getTransaction().commit();
         em.close();
+        return true;
     }
 
-    public void changeStateName(String prevTypeName, String nowTypeName){
+    public boolean changePermissionName(int permId, String nowPermName){
         EntityManager em = EntityService.getEntityManager();
         em.getTransaction().begin();
-        em.createQuery("SELECT t FROM EntityStationsStates t WHERE name = :prevName", EntityPermissionTypes.class)
-                .setParameter("prevName",prevTypeName)
-                .getSingleResult()
-                .setName(nowTypeName);
+        try {
+            em.createQuery("SELECT t FROM EntityStationsStates t WHERE t.id = :id", EntityPermissionTypes.class)
+                    .setParameter("id",permId)
+                    .getSingleResult()
+                    .setName(nowPermName);
+        } catch (Exception e) {
+            return false;
+        }
         em.getTransaction().commit();
         em.close();
+        return false;
+    }
+
+    public Collection<EntityPermissionTypes> getPermTypes(){
+        EntityManager em = EntityService.getEntityManager();
+        return em.createQuery("SELECT pt FROM EntityPermissionTypes pt",EntityPermissionTypes.class).getResultList();
+    }
+
+    public EntityPermissionTypes getPermTypeByName(String name){
+        EntityManager em = EntityService.getEntityManager();
+        return em.createQuery("SELECT pt FROM EntityPermissionTypes pt where pt.name = :name",EntityPermissionTypes.class)
+                .setParameter("name",name)
+                .getSingleResult();
     }
 
 }
