2c2eceff7c355e744d6e847b510d951fe8532b42
SpaceStar <spacestar.ru@gmail.com>
2018-03-26 01:22:21 +0300

    add StationsService

--- src/course/service/StationsService.java
+++ src/course/service/StationsService.java
@@ -1,20 +1,80 @@
 package course.service;
 
+import course.entity.EntityLines;
+import course.entity.EntityStations;
+import course.entity.EntityStationsStates;
+
+import javax.persistence.EntityManager;
+import java.util.Collection;
+
 public class StationsService {
 
-    public void createStation() {
-
+    public boolean createStation(String name, EntityLines line, EntityStationsStates state) {
+        EntityManager em = EntityService.getEntityManager();
+        EntityStations stations = new EntityStations(name, line, state);
+        em.getTransaction().begin();
+        try {
+            em.persist(stations);
+        } catch (Exception e) {
+            return false;
+        }
+        em.getTransaction().commit();
+        em.close();
+        return true;
     }
 
-    public void deleteStation() {
-
+    public boolean deleteStation(int id) {
+        EntityManager em = EntityService.getEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.createQuery("DELETE FROM EntityStations WHERE id = :id")
+                    .setParameter("id", id).executeUpdate();
+        } catch (Exception e){
+            return false;
+        }
+        em.getTransaction().commit();
+        em.close();
+        return true;
     }
 
-    public void changeStationName(){
-
+    public boolean changeStationName(int id, String newName){
+        EntityManager em = EntityService.getEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.createQuery("SELECT s FROM EntityStations s WHERE s.id = :id", EntityStations.class)
+                    .setParameter("id", id).getSingleResult().setName(newName);
+        } catch (Exception e) {
+            return false;
+        }
+        em.getTransaction().commit();
+        em.close();
+        return true;
     }
 
-    public void changeStationState(){
+    public boolean changeStationState(int id, EntityStationsStates newState){
+        EntityManager em = EntityService.getEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.createQuery("SELECT s FROM EntityStations s WHERE s.id = :id", EntityStations.class)
+                    .setParameter("id", id).getSingleResult().setState(newState);
+        } catch (Exception e) {
+            return false;
+        }
+        em.getTransaction().commit();
+        em.close();
+        return true;
+    }
 
+    public Collection<EntityStations> getStations() {
+        EntityManager em = EntityService.getEntityManager();
+        return em.createQuery("SELECT s FROM EntityStations s", EntityStations.class)
+                .getResultList();
+    }
+
+    public EntityStations getStationByName(String name) {
+        EntityManager em = EntityService.getEntityManager();
+        return em.createQuery("SELECT s FROM EntityStations s where s.name = :name", EntityStations.class)
+                .setParameter("name", name)
+                .getSingleResult();
     }
 }
