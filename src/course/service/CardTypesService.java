4d4289121cb160cecfbce9f8cfa9a31544fe1d1f
gromoff97 <gromoff97@mail.ru>
2018-03-25 22:30:44 +0300

    smth1

--- src/course/service/CardTypesService.java
+++ src/course/service/CardTypesService.java
@@ -1,38 +1,67 @@
 package course.service;
 
+import com.sun.xml.wss.impl.c14n.EXC14nStAXReaderBasedCanonicalizer;
 import course.entity.EntityCardTypes;
 import course.entity.EntityNews;
 
+import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import java.sql.Date;
+import java.util.Collection;
 
 public class CardTypesService {
 
-    public void createCardType(String name) {
+    public boolean createCardType(String name) {
         EntityManager em = EntityService.getEntityManager();
         EntityCardTypes cardType = new EntityCardTypes(name);
         em.getTransaction().begin();
-        em.persist(cardType);
+        try {
+            em.persist(cardType);
+        } catch (Exception e) {
+            return false;
+        }
         em.getTransaction().commit();
         em.close();
+        return true;
     }
 
-    public void deleteCardType(int id){
+    public boolean deleteCardType(int id){
         EntityManager em = EntityService.getEntityManager();
         em.getTransaction().begin();
-        em.createQuery("DELETE FROM EntityCardTypes WHERE id = :id")
-                .setParameter("id", id).executeUpdate();
+        try {
+            em.createQuery("DELETE FROM EntityCardTypes WHERE id = :id")
+                    .setParameter("id", id).executeUpdate();
+        } catch (Exception e) {
+            return false;
+        }
         em.getTransaction().commit();
         em.close();
+        return true;
     }
 
-    public void changeCardTypeName(int id, String newName) {
+    public boolean changeCardTypeName(int id, String newName) {
         EntityManager em = EntityService.getEntityManager();
         em.getTransaction().begin();
-        EntityCardTypes cardType = em.createQuery("SELECT t FROM EntityCardTypes t WHERE t.id = :id", EntityCardTypes.class)
-                .setParameter("id", id).getSingleResult();
-        cardType.setName(newName);
+        try {
+            em.createQuery("SELECT t FROM EntityCardTypes t WHERE t.id = :id", EntityCardTypes.class)
+                    .setParameter("id", id).getSingleResult().setName(newName);
+        } catch (Exception e) {
+            return false;
+        }
         em.getTransaction().commit();
         em.close();
+        return true;
+    }
+
+    public Collection<EntityCardTypes> getCardTypes(){
+        EntityManager em = EntityService.getEntityManager();
+        return em.createQuery("SELECT ct FROM EntityCardTypes ct",EntityCardTypes.class).getResultList();
+    }
+
+    public EntityCardTypes getCardTypeByName(String name){
+        EntityManager em = EntityService.getEntityManager();
+        return em.createQuery("SELECT ct FROM EntityCardTypes ct where ct.name = :name",EntityCardTypes.class)
+                .setParameter("name",name)
+                .getSingleResult();
     }
 }
