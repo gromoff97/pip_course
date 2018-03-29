8b3ccceccbc068558f796fc333a38a49979742b3
SpaceStar <spacestar.ru@gmail.com>
2018-03-29 18:22:26 +0300

    LinesResource is EJB

--- src/course/rest/LinesResource.java
+++ src/course/rest/LinesResource.java
@@ -4,12 +4,14 @@ import com.google.gson.Gson;
 import course.service.LinesService;
 
 import javax.ejb.EJB;
+import javax.ejb.Stateless;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+@Stateless
 @Path("lines")
 public class LinesResource {
     @EJB
