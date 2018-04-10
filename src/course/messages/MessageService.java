b2581020678c32f3f3b39d1e4093566f14028134
SpaceStar <spacestar.ru@gmail.com>
2018-04-11 00:13:43 +0300

    add MessageService

---
+++ src/course/messages/MessageService.java
@@ -0,0 +1,45 @@
+package course.messages;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import javax.ejb.Stateless;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+@Stateless
+public class MessageService {
+    private Connection connection;
+    private Channel channel;
+
+    public MessageService() {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try {
+            connection = factory.newConnection();
+            channel = connection.createChannel();
+            channel.exchangeDeclare("logs", "fanout");
+        } catch (IOException | TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg(String message) {
+        try {
+            channel.basicPublish("logs", "", null, message.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        try {
+            channel.close();
+            connection.close();
+        } finally {
+            super.finalize();
+        }
+    }
+}
