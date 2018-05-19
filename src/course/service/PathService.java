eb822be3e062805e83d0cecffacc17e167bce40a
SpaceStar <spacestar.ru@gmail.com>
2018-05-19 16:22:36 +0300

    fix problem with possibility of duplicate paths

--- src/course/service/PathService.java
+++ src/course/service/PathService.java
@@ -23,7 +23,10 @@ public class PathService {
     @PersistenceContext
     private EntityManager em;
 
-    public boolean createPath(EntityStations fromStation, EntityStations toStation, int timeInSec){
+    public boolean createPath(EntityStations fromStation, EntityStations toStation, int timeInSec) {
+        if (getPathFromTo(fromStation, toStation) != null) {
+            return false;
+        }
         EntityPath path = new EntityPath(fromStation, toStation, timeInSec);
         try {
             em.persist(path);
@@ -52,10 +55,23 @@ public class PathService {
     }
 
     public EntityPath getPathFromTo(EntityStations fromSt, EntityStations toSt) {
-        return em.createQuery("SELECT p FROM EntityPath p where p.fromStation = :st1 and p.toStation = :st2", EntityPath.class)
-                .setParameter("st1", fromSt)
-                .setParameter("st2", toSt)
-                .getSingleResult();
+        EntityPath result;
+        try {
+            result = em.createQuery("SELECT p FROM EntityPath p where p.fromStation = :st1 and p.toStation = :st2", EntityPath.class)
+                    .setParameter("st1", fromSt)
+                    .setParameter("st2", toSt)
+                    .getSingleResult();
+        } catch (Exception e1) {
+            try {
+                result = em.createQuery("SELECT p FROM EntityPath p where p.fromStation = :st1 and p.toStation = :st2", EntityPath.class)
+                        .setParameter("st1", toSt)
+                        .setParameter("st2", fromSt)
+                        .getSingleResult();
+            } catch (Exception e2) {
+                result = null;
+            }
+        }
+        return result;
     }
 
     public EntityPath getPathById(int id){
