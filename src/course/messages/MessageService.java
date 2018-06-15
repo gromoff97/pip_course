e151eb530388222dc55f9f1256f46d2da5284eb4
SpaceStar <spacestar.ru@gmail.com>
2018-06-15 13:41:56 +0300

    add randomizer for queue name

--- src/course/messages/MessageService.java
+++ src/course/messages/MessageService.java
@@ -6,19 +6,18 @@ import com.rabbitmq.client.ConnectionFactory;
 
 import javax.ejb.Stateless;
 import java.io.IOException;
+import java.util.Random;
 import java.util.concurrent.TimeoutException;
 
 @Stateless
 public class MessageService {
     private Connection connection;
-    private Channel channel;
 
     public MessageService() {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         try {
             connection = factory.newConnection();
-            channel = connection.createChannel();
         } catch (IOException | TimeoutException e) {
             e.printStackTrace();
         }
@@ -26,9 +25,11 @@ public class MessageService {
 
     public void sendMsg(String message) {
         try {
+            Channel channel = connection.createChannel();
             channel.exchangeDeclare("logs", "fanout");
             channel.basicPublish("logs", "", null, message.getBytes("UTF-8"));
-        } catch (IOException e) {
+            channel.close();
+        } catch (IOException | TimeoutException e) {
             e.printStackTrace();
         }
     }
@@ -36,11 +37,28 @@ public class MessageService {
     public String createQueue() {
         String queueName = "";
         try {
+            Channel channel = connection.createChannel();
             channel.exchangeDeclare("logs", "fanout");
-            queueName = channel.queueDeclare("", false, false, true, null).getQueue();
-            channel.queueBind(queueName, "logs", "");
-        } catch (IOException e) {
-            System.out.println("Couldn't connect.");
+            String queue = "";
+            try {
+                Random random = new Random();
+                String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+                while (true) {
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < 12; i++) {
+                        sb.append(alphabet.charAt(random.nextInt(alphabet.length())));
+                    }
+                    queue = sb.toString();
+                    channel.queueDeclarePassive(queue);
+                }
+            } catch (IOException e) {
+                channel = connection.createChannel();
+                queueName = channel.queueDeclare(queue, false, false, true, null).getQueue();
+                channel.queueBind(queueName, "logs", "");
+                channel.close();
+            }
+        } catch (IOException | TimeoutException e) {
+            System.out.println("Couldn't connect");
         }
         return queueName;
     }
@@ -48,7 +66,6 @@ public class MessageService {
     @Override
     protected void finalize() throws Throwable {
         try {
-            channel.close();
             connection.close();
         } finally {
             super.finalize();
